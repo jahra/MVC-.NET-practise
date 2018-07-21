32909a6e65bbd604d4ecfd4ee91d9d0468b0c6ad
Honza <honzahrabal@seznam.cz>
2018-07-21 10:32:36 +0200

    baby steps

---
+++ MVCRandNumb/MVCRandNumb/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCRandNumb.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            Models.Generator generator = new Models.Generator();
+            ViewBag.Numb = generator.getNumber(); 
+            return View();
+        }
+    }
+}
---
+++ MVCRandNumb/MVCRandNumb/Models/Generator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCRandNumb.Models
+{
+    public class Generator
+    {
+        private Random random = new Random();
+
+        public int getNumber()
+        {
+            return random.Next(100);
+        }
+    }
+}
