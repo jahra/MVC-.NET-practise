8b5bd45e4c2e33a2d296e79e264aef563a58f5ed
Honza <honzahrabal@seznam.cz>
2018-07-21 14:44:11 +0200

    .

---
+++ ModelBindingCalculator/ModelBindingCalculator/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using ModelBindingCalculator.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ModelBindingCalculator.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index(string param)
+        {
+            Calculator calc = new Models.Calculator();
+            ViewBag.Param = param;
+            return View(calc);
+        }
+        // POST : Home
+        [HttpPost]
+        public ActionResult Index(Calculator calc)
+        {
+            if (ModelState.IsValid)
+            {
+                calc.Calculate();
+            }
+            return View(calc);
+        }
+    }
+}
---
+++ ModelBindingCalculator/ModelBindingCalculator/Models/Calculator.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ModelBindingCalculator.Models
+{
+    public class Calculator
+    {
+        [Required(ErrorMessage ="Please enter number")]
+        [Display(Name ="1. number")]
+        public Nullable<int> Number1 { get; set; }        
+        [Required(ErrorMessage ="Please enter number")]
+        [Display(Name ="2. number")]
+        public Nullable<int> Number2 { get; set; }  
+        public Nullable<double> Result { get; set; }
+        [Required(ErrorMessage ="Choose operation")]
+        [Display(Name ="Operation")]
+        public string Operation { get; set; }
+        public List<SelectListItem> PosibleOperations { get; set; }
+
+        public Calculator()
+        {
+            PosibleOperations = new List<SelectListItem>();
+            PosibleOperations.Add(new SelectListItem { Text = "Add", Value = "+"});
+            PosibleOperations.Add(new SelectListItem { Text = "Sub", Value = "-" });
+            PosibleOperations.Add(new SelectListItem { Text = "Multiply", Value = "*" });
+            PosibleOperations.Add(new SelectListItem { Text = "Divide", Value = "/" });
+        }
+
+        public void Calculate()
+        {
+            switch (Operation)
+            {
+                case "+":
+                    Result = Number1 + Number2;
+                    break;
+                case "-":
+                    Result = Number1 - Number2;
+                    break;
+                case "*":
+                    Result = Number1 * Number2;
+                    break;
+                case "/":
+                    try
+                    {
+                        Result = Number1 / Number2;
+                    }
+                    catch (Exception)
+                    {
+                        Result = null;
+                    }
+                    break;                
+            }
+        }
+    }    
+}
